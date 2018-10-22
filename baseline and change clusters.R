854afc5f3051926ff42119d8717f8bf3a9875d2f
Addison Larson <39769450+addisonlarson@users.noreply.github.com>
2018-10-22 09:24:03 -0400

    stylistic updates

--- baseline and change clusters.R
+++ baseline and change clusters.R
@@ -10,13 +10,25 @@ summary(mod)
 plot(mod, what = "density", type = "persp")
 ts$group <- mod$classification
 # st_write(ts, "./tsDataGroups.shp")
-attrib$group <- mod$classification
+attrib$Group <- mod$classification
 set.seed(999)
 samp <- attrib %>%
-  group_by(group) %>%
+  group_by(Group) %>%
   sample_n(15)
 # write.csv(samp, "comparetomap.csv", row.names = FALSE)
-attrib$group <- as.factor(attrib$group)
-# tiff("tractGroupings.tiff", units = "in", width = 10, height = 7.5, res = 600, compression = "lzw")
-ggplot(attrib, aes(x = pct199_16, y = base90_16, color = group)) + geom_point()
-# dev.off()
+attrib$Group <- as.factor(attrib$Group)
+myCols <- c(rgb(123,117,84, maxColorValue = 255),
+            rgb(23,24,59, maxColorValue = 255),
+            rgb(161,22,146, maxColorValue = 255),
+            rgb(114,190,118, maxColorValue = 255),
+            rgb(255,180,154, maxColorValue = 255),
+            rgb(169,175,209, maxColorValue = 255),
+            rgb(255,79,121, maxColorValue = 255))
+tiff("tractGroupings.tiff", units = "in", width = 7.5, height = 7.5, res = 600, compression = "lzw")
+ggplot(attrib, aes(x = pct199_16, y = base90_16, color = Group)) +
+  geom_point() + theme_minimal() +
+  scale_color_manual(values = myCols) +
+  labs(title = "Groupings of Census Tracts by Baseline and Percentage Change",
+       y = "Percentage Change in Low-Income Residents, 1990-2016",
+       x = "Percentage of Low-Income Residents, 2016")
+dev.off()
