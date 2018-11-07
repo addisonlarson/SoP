d5d98df4105eeda96b4b069cfbddff65786e07ef
Addison Larson <39769450+addisonlarson@users.noreply.github.com>
2018-11-07 12:00:56 -0500

    better legends?

--- baseline and change clusters.R
+++ baseline and change clusters.R
@@ -19,20 +19,20 @@ plot(mod, what = "density", type = "persp")
 # 5     Slow          Highest
 ts$origGroup <- mod$classification
 ts$revisedGroup <- factor(recode(mod$classification,
-                   "1 = 'High-High';
-                   2 = 'Moderate-High';
-                   4 = 'Slow-High';
-                   6 = 'Moderate-Low';
-                   7 = 'Slow-Low';
-                   3 = 'Slow-Moderate';
-                   5 = 'Slow-Highest'"),
-                   levels = c("High-High",
-                              "Moderate-High", 
-                              "Slow-High",
-                              "Moderate-Low",
-                              "Slow-Low",
-                              "Slow-Moderate",
-                              "Slow-Highest"))
+                   "1 = 'High change, low overall percentage';
+                   2 = 'Moderate change, low overall percentage';
+                   4 = 'Slow change, low overall percentage';
+                   6 = 'Moderate change, high overall percentage';
+                   7 = 'Slow change, high overall percentage';
+                   3 = 'Slow change, moderate overall percentage';
+                   5 = 'Negative change, low overall percentage'"),
+                   levels = c("High change, low overall percentage",
+                              "Moderate change, low overall percentage", 
+                              "Slow change, low overall percentage",
+                              "Moderate change, high overall percentage",
+                              "Slow change, high overall percentage",
+                              "Slow change, moderate overall percentage",
+                              "Negative change, low overall percentage"))
 # st_write(ts, "./tsDataGroups.shp")
 attrib$Group <- ts$revisedGroup
 set.seed(999)
@@ -46,12 +46,12 @@ myCols <- c(rgb(101,129,102, maxColorValue = 255),
             rgb(79,157,193, maxColorValue = 255),
             rgb(170,216,224, maxColorValue = 255),
             rgb(230,230,230, maxColorValue = 255))
-tiff("tractGroupings.tiff", units = "in", width = 7.5, height = 7.5, res = 600, compression = "lzw")
+tiff("tractGroupings.tiff", units = "in", width = 10, height = 8, res = 600, compression = "lzw")
 ggplot(attrib, aes(x = pct199_16, y = base90_16, color = Group)) +
   geom_point() + theme_minimal() +
   scale_color_manual(values = myCols) +
-  labs(title = "Groupings of Census Tracts by Baseline and Percentage Change",
-       subtitle = "First word describes rate of change; second describes tract income composition",
-       y = "Percentage Change in Low-Income Residents, 1990-2016",
-       x = "Percentage of Low-Income Residents, 2016")
+  labs(title = "Census tract typologies",
+       subtitle = "Low-income is defined as households with incomes below 200% of the Federal Poverty Level.",
+       y = "Rate of Change in Low-Income Residents, 1990-2016",
+       x = "Percentage Low-Income Residents, 2016")
 dev.off()
